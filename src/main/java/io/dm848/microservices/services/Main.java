9bbbc7513dd78315c54060590655b0d28260de2a
crashh <lthomasen@gmail.com>
2016-06-02 10:59:46 +0200

    Initial commit.

---
+++ src/main/java/io/dm848/microservices/services/Main.java
@@ -0,0 +1,53 @@
+package io.dm848.microservices.services;
+
+import io.dm848.microservices.services.accounts.AccountsServer;
+import io.dm848.microservices.services.registration.RegistrationServer;
+import io.dm848.microservices.services.web.WebServer;
+
+/**
+ * Only purpose of this file is to allow the services to be started from
+ * command-line.
+ * Has to be the <code>Main-Class</code> in the jar's <code>MANIFEST.MF</code>.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		String serverName = "NO-VALUE";
+
+		for (int i=0; i<args.length; i+=2) {
+			switch (args[i]) {
+				case "-port":
+					// Optionally set the HTTP port to listen on, overrides
+					// value in the <server-name>-server.yml file
+					System.setProperty("server.port", args[i+1]);
+					// Fall through into ..
+
+				case "-name":
+					serverName = args[i+1].toLowerCase();
+					break;
+
+				default:
+					usage();
+					return;
+			}
+		}
+
+
+		if (serverName.equals("registration") || serverName.equals("reg")) {
+			RegistrationServer.main(args);
+		} else if (serverName.equals("accounts") || serverName.equals("acc")) {
+			AccountsServer.main(args);
+		} else if (serverName.equals("webserver") || serverName.equals("web")) {
+			WebServer.main(args);
+		} else {
+			System.out.println("Unknown server name: " + serverName);
+			usage();
+		}
+	}
+
+	private static void usage() {
+		System.out.println("Usage: java -jar ... -name <server-name> -port [server-port]");
+		System.out.println("       where server-name is 'reg', 'registration', " + "'accounts' or 'webserver' and server-port > 1024");
+	}
+}
